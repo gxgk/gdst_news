01fa58a54bd946825f9d918c3e8629edfcc3f16a
ikksss <571479341@qq.com>
2018-11-15 04:21:13 -0800

    something change

--- __init__.py
+++ __init__.py
@@ -1,16 +1,20 @@
-from flask import Flask
-import json
-from . import school_news as sn
+from flask import Flask, jsonify
+import school_news as sn
+
 
 app = Flask(__name__)
 
 
-@app.route('/api/SearchBook', methods=['GET'])
+@app.route('/api/GetNews', methods=['GET'])
 def spider():
     news_detail = sn.get_news_detail(sn.get_news())
     notice_detail = sn.get_notice_detail(sn.get_notice())
 
-    return json.dump({
+    return ({
         'news_detail': news_detail,
         'notice_detail': notice_detail,
     })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
