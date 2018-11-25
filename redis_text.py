053c267042e1059515cde14ac28d40cd44daf6ba
ikksss <571479341@qq.com>
2018-11-24 23:06:24 -0800

    对redis_decorator模块做点了修改

--- redis_text.py
+++ redis_text.py
@@ -5,62 +5,53 @@ import re
 def new_cache():
     def decorator(func):
         def wrapper(*args, **kw):
-            if r.get(
-                    func.__name__ +
-                    'xy') is None or r.exists(
-                    func.__name__ +
-                    'xy'):
-                # 判断缓存过期或者第一次缓存
-                if func.__name__ == 'get_news' or func.__name__ == 'get_notice':
+            if func.__name__ == 'get_news' or func.__name__ == 'get_notice':
+                if r.get(
+                        func.__name__ +
+                        'xy') is None or r.exists(
+                        func.__name__ +
+                        'xy'):
+                    # 判断缓存是否过期或是否是第一次缓存
                     data = func()
-                    r.set(func.__name__ + '_list', str(data))
+                    r.set('old_list', str(data))
+                    r.expire('old_list', 60 * 2880)
+                    # 储存data集合 用于验证是否有新增与get_xxx_detail函数使用
                     for content in data:
-                        r.rpush(
+                        r.lpush(
                             func.__name__ +
                             content['origin'],
                             str(content))
-                        r.expire(func.__name__ + content['origin'], 60 * 2880)
-                        # 列表缓存过期时间为两天
-                else:
+                        r.expire(func.__name__ + content['origin', 60 * 2880])
+                        # 列表缓存时间为两天
 
-                    data = func(eval(r.get(func.__name__ + 'list')))
-                    for content in data:
-                        r.rpush(
+                data = func()
+                if data != eval(r.get('list')):
+                    r.set('new_list', str(data))
+                    r.expire('new_list', 60 * 2880)
+                # 判断是否有新增
+                    for content not in data:
+                        list = r.get(func.__name__ + content['origin'])
+                        r.lpush(
                             func.__name__ +
                             content['origin'],
                             str(content))
                         r.expire(func.__name__ + content['origin'], 60 * 2880)
-                        # 详细缓存过期时间为三天
 
             else:
-                # 若缓存为过期则判断有无新增
-                if func.__name__ == 'get_news'or func.__name__ == 'get_notice':
-                    data = func()
-                try:
-                    # 只有当被修饰的函数是get_news()或get_notice()才执行
+                if r.get(
+                        func.__name__ +
+                        'xy') is None or r.exists(
+                        func.__name__ +
+                        'xy') or r.get('old_list') != r.get('new_list'):
+                    # 若old_list与new_list不相同则表示有新增内容 也重新获取
+                    data = func(eval(r.get('new_list')))
                     for content in data:
-                        old_list = r.get(func.__name__ + content['origin'])
-                        if content not in old_list:
-                            r.rpush(
-                                func.__name__ +
-                                content['origin'],
-                                str(content))
-
-                except BaseException:
-                    pass
-
-                else:
-                    data = []
-                    for type in [
-                        'xy',
-                        'jxjx',
-                        'cjx',
-                        'ysx',
-                        'yyx',
-                        'glx',
-                        'jdx',
-                    ]:
-                        data.append(eval(r.get(func.__name__ + type)))
+                        r.lpush(
+                            func.__name__ +
+                            content['origin'],
+                            str(content))
+                        r.expire(func.__name__ + content['origin'], 60 * 4320)
+                        # 详细缓存时间为三天
 
             return data
         return wrapper
