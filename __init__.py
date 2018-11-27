3cfd0c7951b205a988ff448705a7eb5f0a403d1a
ikksss <571479341@qq.com>
2018-11-27 04:01:16 -0800

    更改返回的接口参数,redis装饰器做了一些修改

--- __init__.py
+++ __init__.py
@@ -11,13 +11,30 @@ r = redis.Redis(connection_pool=pool)
 
 @app.route('/api/GetNews', methods=['GET'])
 def spider():
-    news_detail = sn.get_news_detail(sn.get_news_detail)
-    notice_detail = sn.get_notice_detail(sn.get_notice_detail)
-    return json.dumps({
-        'news_dateil': news_detail,
-        'notice_dateil'notice_detail,
-    })
+    news_list = sn.get_news()
+    notice_list = sn.get_notice()
+    news_detail = sn.get_news_detail(news_list)
+    notice_detail = sn.get_notice_detail(notice_list)
+    xy_newslist = []
+    xy_newsdetail = []
 
+    for content in news_list:
+        if content['origin'] == 'xy':
+            xy_newslist.append(content)
+            news_list.remove(content)
+
+    for content in news_detail:
+        if content['origin'] == 'xy':
+            xy_newsdetail.append(content)
+            news_list.remove(content)
+
+    return json.dumps({
+        'xy_news_list':xy_newslist,
+        'xy_news_detail':xy_newsdetail,
+        'orther_news':news_list,
+        'notice_list':notice_list,
+        'notice_detail':notice_detail,
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, port=8848)
