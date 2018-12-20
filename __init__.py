0742066c8e18fb53c2c3c5970317c12b04555076
ikksss <571479341@qq.com>
2018-12-20 08:46:38 -0800

    重构

--- __init__.py
+++ __init__.py
@@ -2,50 +2,29 @@ from flask import Flask
 import json
 import school_news as sn
 import redis
-from flask import request
 
 
 app = Flask(__name__)
-pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
-r = redis.Redis(connection_pool=pool)
-
-def spider():
-    news_list = sn.get_news()
-    notice_list = sn.get_notice()
-    news_detail = sn.get_news_detail(news_list)
-    notice_detail = sn.get_notice_detail(notice_list)
-
-    sn.data_classify(news_list)
-    sn.data_classify(notice_list)
-    sn.data_classify(news_detail)
-    sn.data_classify(notice_detail)
-
-@app.route('/result/<type>/page=<page>',method="GET")
-def return_result(type,page):
-    if request.method == "GET":
-        m = int(page)*10-10
-        n = int(page)*10-1
-        try:
-            data = r.get('type')[m:n]
-            return json.dumps({
-                'data':data,
-                'content':'上滑获取更多'
-            })
-        except:
-            if r.get('type')[m] is False:
-                return json.dumps({'data':'没有再多的了！'})
-            else:
-                while r.get('type')[m]:
-                    m+=1
-
-                data = r.get('type')[n:m-1]
-                return json.dumps({
-                    'data': data,
-                    'content': '没有再多的了！'
-                })
 
 
+@app.rout('/<type>/<origin>/?page=<page>', method='GET')
+def get_list_api():
+    # 获取新闻或通告列表
+    if type == 'news':
+        list = sn.get_news(origin, page)
+    else:
+        list = sn.get_notice(origin, page)
+
+    return json.dumps(list)
 
 
+@app.rout('/<type>/<origin>/<url>', method='GET')
+# 接受新闻内容URL
+def get_detail_api():
+    # 获取新闻或者通告详细
+    if type == "news":
+        detail = sn.get_news_detail(url)
+    if type == "notice":
+        detail = sn.get_notice_detail(url)
 
-
+    return json.dumps(detail)
--- school_news.py
+++ school_news.py
@@ -2,203 +2,123 @@ import requests
 from bs4 import BeautifulSoup
 import logging
 import time
-from decorator import  judge_list,judge_detail,new_cache
+from fake_useragent import UserAgent
 
-@judge_list()
-def get_news():
-    news_url = []
-    news_list = []
-    news_type = {
-        'xy': '/news/syyw/',
-        'jsjx': '/jsjx/xbxw/xbdt/',
-        'cjx': '/cjx/xbxw/xbdt/',
-        'ysx': '/ysx/xbxw/xbdt/',
-        'yyx': '/yyx/xbxw/xbdt/',
-        'glx': '/glx/xbxw/xbdt/',
-        'jdx': '/jdx/xbxw/xbdt/',
-    }
+ua = UserAgent(verify_ssl=False)
+# 生成USER-ANGENT
 
-    for key in news_type:
-        for n in range(1, 4):
-            url = 'http://www.gdst.cc' + news_type[key] + '?page=' + str(n)
+news_type = {
+    'xy': '/news/syyw/?page=',
+    'jsjx': '/jsjx/xbxw/xbdt/?page=',
+    'cjx': '/cjx/xbxw/xbdt/?page=',
+    'ysx': '/ysx/xbxw/xbdt/?page=',
+    'yyx': '/yyx/xbxw/xbdt/?page=',
+    'glx': '/glx/xbxw/xbdt/?page=',
+    'jdx': '/jdx/xbxw/xbdt/?page=',
+}
+notice_tyoe = {
+    'xy': '/news/tzgg/',
+    'jsjx': '/jsjx/xbxw/tzgg/?page=',
+    'cjx': '/cjx/xbxw/tzgg/?page=',
+    'ysx': '/ysx/xbxw/tzgg/?page=',
+    'yyx': '/yyx/xbxw/tzgg/?page=',
+    'glx': '/glx/xbxw/tzgg/?page=',
+    'jdx': '/jdx/xbxw/tzgg/?page=',
+}
+
+news_list = []
+notice_list = []
+
+
+def get_news(origin, page=1):
+    # 获取新闻列表,接受前端的请求的来源（院别）,页数默认为1，新闻获取数量为15条
+    url = 'http://www.gdust.cn/' + news_type[origin] + str(page)
+    try:
+        headers = {'user-agent': ua.chrome}
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, "html.parser")
+        rows = soup.find(class_='article').find_all('li')
+    except Exception as e:
+        logging.warning(u'学院官网连接超时错误:%s' % e)
+        return {}
+    else:
+        for row in rows:
+            date = row.find(class_='date')
+            # 匹配时间
+            date = date.getText()
+            date.replace('/', '-')
+            title = row.a.string
+            url = row.a.attrs['href']
             data = {
-                'url': url,
-                'type': key,
+                'title': title,
+                'url': u'http://www.gdst.cc/' + url,
+                'date': date,
+                'origin': origin,
             }
-            news_url.append(data)
+            news_list.append(data)
 
-    for content in news_url:
-        try:
-            r = requests.get(content['url'])
-            soup = BeautifulSoup(r.text, "html.parser")
-            rows = soup.find(class_='article').find_all('li')
-        except Exception as e:
-            logging.warning(u'学院官网连接超时错误:%s' % e)
-            return {}
-        else:
-            for row in rows:
-                date = row.find(class_='date')
-                # 匹配时间
-                date = date.getText()
-                date.replace('/', '-')
-                title = row.a.string
-                url = row.a.attrs['href']
-                data = {
-                    'title': title,
-                    'url': u'http://www.gdst.cc/' + url,
-                    'date': date,
-                    'origin': content['type'],
-                }
-                news_list.append(data)
-            time.sleep(2)
+    return news_list
 
-    return {
-        'data':news_list,
-        'type':'news_list',
-    }
 
-@judge_list()
-def get_notice():
-    notice_url = []
-    notice_list = []
-    notice_type = {
-        'xy': '/news/tzgg/',
-        'jsjx': '/jsjx/xbxw/tzgg/',
-        'cjx': '/cjx/xbxw/tzgg/',
-        'ysx': '/ysx/xbxw/tzgg/',
-        'yyx': '/yyx/xbxw/tzgg/',
-        'glx': '/glx/xbxw/tzgg/',
-        'jdx': '/jdx/xbxw/tzgg/',
-    }
-
-    for key in notice_type:
-        # 构建URL
-        for n in range(1, 4):
-            url = 'http://www.gdst.cc' + \
-                notice_type[key] + '?page=' + str(n)
+def get_notice(origin, page=1):
+    # 获取通告列表，接受前端的请求的来源（院别）,页数默认为1，新闻获取数量为15条
+    url = 'http://www.gdust.cn/' + notice_list[origin] + str(page)
+    try:
+        headers = {'user-agent': ua.chrome}
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, "html.parser")
+        rows = soup.find(class_='article').find_all('li')
+    except Exception as e:
+        logging.warning(u'学院官网连接超时错误:%s' % e)
+        return {}
+    else:
+        for row in rows:
+            date = row.find(class_='date')
+            # 匹配时间
+            date = date.getText()
+            date.replace('/', '-')
+            title = row.a.string
+            url = row.a.attrs['href']
             data = {
-                'url': url,
-                'type': key,
+                'title': title,
+                'url': u'http://www.gdst.cc/' + url,
+                'date': date,
+                'origin': origin,
             }
-            notice_url.append(data)
+            notice_list.append(data)
 
-    for content in notice_url:
-        try:
-            r = requests.get(content['url'])
-            soup = BeautifulSoup(r.text, "html.parser")
-            rows = soup.find(class_='article').find_all('li')
-        except Exception as e:
-            logging.warning(u'学院官网连接超时错误:%s' % e)
-            return {}
-        else:
-            for row in rows:
-                date = row.find(class_='date')
-                # 匹配时间
-                date = date.getText()
-                date.replace('/', '-')
-                title = row.a.string
-                url = row.a.attrs['href']
-                data = {
-                    'title': title,
-                    'url': u'http://www.gdst.cc/' + url,
-                    'date': date,
-                    'origin': content['type'],
-                }
-                notice_list.append(data)
-            time.sleep(2)
-
-    return {
-        'data':notice_list,
-        'type':'notice_list',
-    }
+    return notice_list
 
 
-@judge_detail(get_news())
-def get_news_detail(news_list):
-    news_detail = []
-    for content in news_list:
-        try:
-            r = requests.get(content['url'])
-            soup = BeautifulSoup(r.text, 'html.parser')
-            rows = soup.find(class_='articleinfor')
-        except Exception as e:
-            logging.warning(u'学院官网连接超时错误:%s' % e)
-            return {}
-        else:
-            data = {
-                'title': content['title'],
-                'origin': content['origin'],
-                'html': rows,
-            }
-            news_detail.append(data)
-            time.sleep(2)
+def get_news_detail(url):
+    # 获取新闻详细
+    try:
+        headers = {'user-agent': ua.chrome}
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        rows = soup.find(class_='articleinfor')
+    except Exception as e:
+        logging.warning(u'学院官网连接超时错误:%s' % e)
+        return {}
+    else:
+        data = rows
 
-    return {
-        'data':news_detail,
-        'type':'news_detail',
-    }
+    return rows
 
 
-@judge_detail(get_notice())
-def get_notice_detail(notice_list):
-    notice_detail = []
-    for content in notice_list:
-        try:
-            r = requests.get(content['url'])
-            soup = BeautifulSoup(r.text, 'html.parser')
-            rows = soup.find(class_='articleinfor')
-        except Exception as e:
-            logging.warning(u'学院官网连接超时错误:%s' % e)
-            return {}
-        else:
-            data = {
-                'title': content['title'],
-                'origin': content['origin'],
-                'html': rows,
-            }
-            notice_detail.append(data)
-            time.sleep(2)
+def get_notice_detail(url):
+    # 获取通告详细
+    try:
+        headers = {'user-agent': ua.chrome}
+        r = requests.get(content['url'], headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        rows = soup.find(class_='articleinfor')
+    except Exception as e:
+        logging.warning(u'学院官网连接超时错误:%s' % e)
+        return {}
+    else:
+        data = rows
+    return rows
 
-    return {
-        'data':notice_detail,
-        'type':'notice_detail'
-    }
 
-@new_cache()
-def data_classify(dic):
-    xy = []
-    jsjx = []
-    cjx = []
-    ysx = []
-    yyx = []
-    jdx = []
-    glx = []
-    type = dic['type']
-    list = dic['data']
-    for data in list:
-        if data['origin'] == "xy":
-            xy.append(data)
-        elif data['origin'] == "jsjx":
-            jsjx.append(data)
-        elif data["origin"] == "cjx":
-            cjx.append(data)
-        elif data["origin"] == "ysx":
-            ysx.append(data)
-        elif data["origin"] == "yyx":
-            yyx.append(data)
-        elif data['origin'] == "jdx":
-            jdx.append(data)
-        else:
-            glx.append(data)
-
-    return {
-        'type':type,
-        'xy':xy,
-        'jsjx':jsjx,
-        'cjx':cjx,
-        'ysx':ysx,
-        'yyx':yyx,
-        'jdx':jdx,
-        'glx':glx,
-    }
 
