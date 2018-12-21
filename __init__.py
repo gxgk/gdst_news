38adff2fd989231219d9623ef6a9af26d99df867
WeijiangChen <ourweijiang@gmail.com>
2018-12-22 03:21:23 +0800

    fix bug

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 import json
 import school_news as sn
-import redis
 
 
 app = Flask(__name__)
 
 
-@app.rout('/<type>/<origin>/?page=<page>', method='GET')
-def get_list_api():
+@app.route('/<type>/<origin>', methods=['GET'])
+def get_list_api(type, origin):
+    page = request.args.get('page')
     # 获取新闻或通告列表
     if type == 'news':
         list = sn.get_news(origin, page)
@@ -18,9 +18,9 @@ def get_list_api():
     return json.dumps(list)
 
 
-@app.rout('/<type>/<origin>/<url>', method='GET')
+@app.route('/<type>/<origin>/<url>', methods=['GET'])
 # 接受新闻内容URL
-def get_detail_api():
+def get_detail_api(type, origin, url):
     # 获取新闻或者通告详细
     if type == "news":
         detail = sn.get_news_detail(url)
