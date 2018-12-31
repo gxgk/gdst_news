98bed6f338d3a9545ebe534410d1453cc6d69e1a
ikksss <571479341@qq.com>
2018-12-31 00:38:31 -0800

    资讯 flask app

---
+++ app/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import json
+import school_news as sn
+from urllib.parse import unquote
+import re
+app = Flask(__name__)
+
+
+@app.route('/news/list', methods=['GET'])
+def get_list_api():
+    news_type = request.args.get('news_type')
+    page = request.args.get('page')
+    faculty = request.args.get('faculty')
+    faculty = unquote(faculty)
+    # 获取新闻或通告列表
+    list = sn.get_news(news_type, faculty, page)
+
+    return json.dumps({
+        'status': 200,
+        'data': list,
+    })
+
+
+@app.route('/news/detail', methods=['GET'])
+# 接受新闻内容URL
+def get_detail_api():
+    url = request.args.get('url')
+    # 获取新闻或者通告详细
+    url = unquote(url)
+    try:
+        if re.search('http://jwc.gdst.cc/', url)[0]:
+            detail = sn.get_notice_detail(url)
+    except BaseException:
+        detail = sn.get_news_detail(url)
+
+    return json.dumps({'status': 200, 'data': detail})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
