7217808f55594fcbbf8a44f6542d43c89bd340d3
ikksss <571479341@qq.com>
2019-01-17 00:21:18 -0800

    fix bug

--- app/school_news/school_news.py
+++ app/school_news/school_news.py
@@ -85,7 +85,7 @@ def get_news_detail(url):
             date = re.search('\d.*\d', str(date))[0]
             content = rows.find(class_="content")
             content = str(content).replace(
-                'src="/', 'src="http://www.gdust.cn/')
+                "src=\"/", "src=\"http://www.gdust.cn/")
             content = b64encode(content.encode())
 
     return {
