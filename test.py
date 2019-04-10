da1ddf54c33bb38afea377ed1325d83c634b33d3
ikksss <571479341@qq.com>
2019-04-10 05:09:32 -0700

    revert test.py

--- test.py
+++ test.py
@@ -32,35 +32,18 @@ class TestCase(unittest.TestCase):
                 faculty = ''
 
             for page in range(1, 2):
-                qurey_url_test = "%s?news_type=%s&page=%s&faculty=%s&force_reload=%s" % (
+                qurey_url = "%s?news_type=%s&page=%s&faculty=%s&force_reload=%s" % (
                     config.LIST_URL, origin, page, faculty, '1')
-
-                qurey_url = "%s?news_type=%s&page=%s&faculty=%s" %(config.LIST_URL, origin, page, faculty)
                 if key == 'xm':
                     for gzh_name in config.NEWS_TYPE['xm']:
                         qurey_url += '&gzh_name=%s' % gzh_name
-                        qurey_url_test += '&gzh_name=%s' % gzh_name
                         self.app.get(qurey_url)
-                        self.app.get(qurey_url_test)
                         print(qurey_url)
-                        print(qurey_url_test)
                 else:
                     ret = self.app.get(qurey_url)
-                    ret_test = self.app.get(qurey_url_test)
                     data = bytes.decode(ret.data)
-                    data_test = bytes.decode(ret_test.data)
                     print(qurey_url)
-                    print(qurey_url_test)
                     for content in ast.literal_eval(data)['data']:
-                        url = content['url']
-                        self.app.get(
-                            "%s?type=%s&url=%s&force_reload=%s" %
-                            (config.DETAIL_URL, origin, url, '1'))
-                        print(url)
-                        time.sleep(1)
-                    time.sleep(1)
-
-                    for content in ast.literal_eval(data_test)['data']:
                         url = content['url']
                         self.app.get(
                             "%s?type=%s&url=%s&force_reload=%s" %
