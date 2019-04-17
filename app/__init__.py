f514f352e57680e5a9631b187c60261a72efee3a
ikksss <571479341@qq.com>
2019-04-17 07:26:26 -0700

    已连接sentry

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_redis import FlaskRedis
 from raven.contrib.flask import Sentry
+import config
+
 redis_store = FlaskRedis()
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config')
-    sentry = Sentry(app, dsn='https://9f470bbae23145b1b1ad402405ad310d:f7a19ed055d340508bdac85f71427b46@sentry.gxgk.cc/5')
+    sentry = Sentry(app, dsn=config.SENTRY_DNS)
 
     from app.school_news import school_news_mod
     app.register_blueprint(school_news_mod)
