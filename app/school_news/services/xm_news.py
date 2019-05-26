10bd6dc3b15e1ce5dc6446911ab15b4b6bc9c855
WeijiangChen <ourweijiang@gmail.com>
2019-05-26 20:00:34 +0800

    修复低级bug

--- app/school_news/services/xm_news.py
+++ app/school_news/services/xm_news.py
@@ -1,8 +1,7 @@
 import requests
 import logging
 from app import redis_store
-from bs4 import BeautifulSoup
-import base64
+import pickle
 import ast
 from urllib.parse import quote,unquote
 
@@ -12,17 +11,13 @@ redis_plugin_prefix = 'wechat:plugins:news:xiaomiao'
 def update_cache():
     url = 'http://mp.weixin.qq.com/mp/homepage' \
         '?__biz=MzI1MzA1MzQ0MA==&hid=3' \
-        '&sn=2058fc67f54c5b913396dab4db8149ff'
-
-    session = requests.Session()
-    session.headers.update({
+        '&sn=2058fc67f54c5b913396dab4db8149ff&begin=0&count=29&action=appmsg_list&f=json'
+    headers = {
         'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; ' +
         'Windows NT 6.2; Trident/6.0)'
-    })
+    }
     try:
-        res = session.get(url, timeout=6)
-        url = url + '&begin=0&count=29&action=appmsg_list&f=json'
-        res = session.post(url, timeout=6)
+        res = requests.get(url, timeout=6, headers=headers)
         appmsg_list = res.json()['appmsg_list']
     except Exception as e:
         logging.warning(u'连接超时出错：%s' % e)
@@ -35,8 +30,8 @@ def update_cache():
             "type": "xm",
         }
         news_list.append(news_data)
-
-        redis_store.set(redis_plugin_prefix + ':list', news_list, 3600 * 10)
+    redis_store.set(redis_plugin_prefix + ':list', pickle.dumps(news_list), 3600 * 10)
+    return news_list
 
 
 def get_list(page=1):
@@ -44,10 +39,10 @@ def get_list(page=1):
     if page >= 3:
         return {'end': ''}
     list_cache = redis_store.get(redis_plugin_prefix + ':list')
-    if not list_cache:
-        update_cache()
-        list_cache = redis_store.get(redis_plugin_prefix + ':list')
-    cache = ast.literal_eval(str(list_cache, encoding='utf-8'))
+    if list_cache:
+        cache = pickle.dumps(list_cache)
+    else:
+        cache = update_cache()
     cache_list = []
     for i in range(0, len(cache), 10):
         cache_list.append(cache[i:i + 10])
@@ -56,6 +51,6 @@ def get_list(page=1):
 
 def get_detail(url):
     return {
-        'status':200,
-        'url':unquote(url)
+        'status': 200,
+        'url': unquote(url)
     }
