d38d8dab17d774f0d0ce7f8b833c5ccec7d2822f
ikksss <572179341@qq.com>
2019-06-09 18:34:12 +0800

    增加严选干货

--- app/school_news/services/xm_news.py
+++ app/school_news/services/xm_news.py
@@ -8,27 +8,32 @@ redis_plugin_prefix = 'wechat:plugins:news:xiaomiao'
 
 
 def update_cache():
-    url = 'http://mp.weixin.qq.com/mp/homepage?__biz=MzI1MzA1MzQ0MA==&' \
-          'hid=3&sn=2058fc67f54c5b913396dab4db8149ff&begin=0&count=29&action=appmsg_list&f=json'
+    urls = ['http://mp.weixin.qq.com/mp/homepage?__biz=MzI1MzA1MzQ0MA==&' \
+          'hid=3&sn=2058fc67f54c5b913396dab4db8149ff&begin=0&count=29&action=appmsg_list&f=json',
+           'https://mp.weixin.qq.com/mp/homepage?__biz=MzU1NDkwNDY5MQ==&'\
+           'hid=1&sn=d19338069924da83163881e59cc771ca&begin=0&count=29&action=appmsg_list&f=json'
+           ]
     headers = {
         'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; ' +
         'Windows NT 6.2; Trident/6.0)'
     }
-    try:
-        res = requests.get(url, timeout=6, headers=headers)
-        appmsg_list = res.json()['data']['homepage_render']['appmsg_list']
-    except Exception as e:
-        logging.warning(u'连接超时出错：%s' % e)
-        return {}
     news_list = []
-    for appmsg in appmsg_list:
-        news_data = {
-            "title": appmsg['title'],
-            "url": quote(appmsg['link']),
-            "author": appmsg['author'],
-            "type": "xm",
-        }
-        news_list.append(news_data)
+    for url in urls:
+        try:
+            res = requests.get(url, timeout=6, headers=headers)
+            appmsg_list = res.json()['data']['homepage_render']['appmsg_list']
+        except Exception as e:
+            logging.warning(u'连接超时出错：%s' % e)
+            return {}
+
+        for appmsg in appmsg_list:
+            news_data = {
+                "title": appmsg['title'],
+                "url": quote(appmsg['link']),
+                "author": appmsg['author'],
+                "type": "xm",
+            }
+            news_list.append(news_data)
     redis_store.set(redis_plugin_prefix, pickle.dumps(news_list), 3600 * 10)
     return news_list
 
